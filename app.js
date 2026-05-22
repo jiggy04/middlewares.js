bb535112f97506bf1651f9b79fdc47ee87e12069
Gideon Iliya <ogah2.il@gmail.com>
2026-05-22 12:10:54 +0100

    moved middlewares into one folder

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ require("dotenv").config()
 const express = require('express');
 const app = express();
 const cors = require("cors")
-const logRequest = require('./logger')
+const logRequest = require('./middlewares/logger')
 const validateTodo = require('./middlewares/validator')
 const errorhandler = require('./middlewares/errorHandler')
 
